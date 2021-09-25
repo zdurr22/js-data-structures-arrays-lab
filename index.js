65adc7654f6404491382ca3bc3f6bc10e84eb476
zdurr22 <zdurr22@gmail.com>
2021-09-25 15:18:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+append.push('Odie');
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.unshift('Odie');
+const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop();
+const removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift();
